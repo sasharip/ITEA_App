e8a2f67fef21a928bcead8548bf62e5a162ff7d4
vattghern777 <vattghern777@gmail.com>
2016-11-24 20:05:54 +0200

    Added button

--- app/src/main/java/com/vatttghern/itea_app/MainActivity.java
+++ app/src/main/java/com/vatttghern/itea_app/MainActivity.java
@@ -2,17 +2,25 @@ package com.vatttghern.itea_app;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button bGitText;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        bGitText = findViewById(R.id.bGitText);
+        bGitText = (Button) findViewById(R.id.bGitText);
 
-        
+        bGitText.setOnClickListener(new View.OnClickListener() {
 
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Git button clicked", Toast.LENGTH_SHORT).show();
+            }
+        }
     }
-}
